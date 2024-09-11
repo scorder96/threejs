f886031f26f3c9095561a1b96032e902b4cfe981
scorder96 <scorder96@gmail.com>
2024-09-11 11:34:41 +0530

    scene update

--- src/Scene3D.tsx
+++ src/Scene3D.tsx
@@ -38,7 +38,7 @@ function Scene3D() {
   const [Loading, setLoading] = useState(String);
   const loader = new GLTFLoader();
   loader.load(
-    "/railwayst1.gltf",
+    "/railwayst1light.glb",
     function (gltf) {
       // gltf.scene.scale.set(4, 4, 4);
       scene.add(gltf.scene);
